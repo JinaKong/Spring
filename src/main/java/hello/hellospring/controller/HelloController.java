febff8a48c902fff9b6808d6e4566ccce1d77d9c
JinaKong <kongjn226@gmail.com>
2023-06-18 03:07:24 +0900

    Add MVC page

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller //스프링부트에서는 컨트롤러에 이 표시를 달아주어야 한다.
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute( "data","hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
