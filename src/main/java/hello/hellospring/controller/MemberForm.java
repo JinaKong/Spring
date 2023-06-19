a30c68aca466979606142c7f443430ca37e09aba
JinaKong <kongjn226@gmail.com>
2023-06-20 02:06:19 +0900

    Add Join page

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    //getter
+    public String getName(){
+        return name;
+    }
+
+    //setter
+    public void setName(String name){
+        this.name = name;
+    }
+}
